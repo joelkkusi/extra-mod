d7277d58f9d3f5fef896eb621818e45d8cbc89b0
Joel Kusi <joelkkusi@gmail.com>
2024-08-12 14:34:30 +0200

    Initial commit

---
+++ src/main/java/dev/joelkkusi/extra_mod/ExtraModClient.java
@@ -0,0 +1,11 @@
+package dev.joelkkusi.extra_mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ExtraModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/dev/joelkkusi/extra_mod/ExtraModDataGenerator.java
@@ -0,0 +1,11 @@
+package dev.joelkkusi.extra_mod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ExtraModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
