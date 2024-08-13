7eac229080c95c9875492b1ed3df1215518cba0f
Joel Kusi <joelkkusi@gmail.com>
2024-08-13 03:37:29 +0200

    Added Pink Garnet & Raw Pink Garnet

--- src/main/java/dev/joelkkusi/extra_mod/ExtraMod.java
+++ src/main/java/dev/joelkkusi/extra_mod/ExtraMod.java
@@ -1,5 +1,6 @@
 package dev.joelkkusi.extra_mod;
 
+import dev.joelkkusi.extra_mod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -17,7 +18,7 @@ public class ExtraMod implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("Loading ExtraMod...");
+		ModItems.registerModItems();
 	}
 }
